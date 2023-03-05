acb24647af0d6ba55e085b9cd85fbc8930e4af57
CMDR-Piboy314 <92357316+CMDR-Piboy314@users.noreply.github.com>
2023-03-05 18:55:54 -0500

    Enable netrw

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,7 +1,3 @@
--- Disable netrw at the very start of your init.lua (strongly advised)
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 -- Set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
